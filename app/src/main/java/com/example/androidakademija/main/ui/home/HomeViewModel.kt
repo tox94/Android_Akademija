7428e4c58d287446c08dbbe4cbe77850cdd9bcde
tox94 <tonibakarcic@gmail.com>
2021-03-03 13:25:04 +0100

    Minor change.

--- app/src/main/java/com/example/androidakademija/main/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/androidakademija/main/ui/home/HomeViewModel.kt
@@ -3,12 +3,11 @@ package com.example.androidakademija.main.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.androidakademija.examples.functions.KotlinFunctionExamples.getRandomNumber
 
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment" + getRandomNumber()
+        value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
 }
