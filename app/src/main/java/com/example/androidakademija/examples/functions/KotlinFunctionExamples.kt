bf2e547395f6c7261a0a4fd716aba8205445430c
tox94 <tonibakarcic@gmail.com>
2021-03-03 16:40:39 +0100

    Functions examples created.

--- app/src/main/java/com/example/androidakademija/examples/functions/KotlinFunctionExamples.kt
+++ app/src/main/java/com/example/androidakademija/examples/functions/KotlinFunctionExamples.kt
@@ -1,22 +1,37 @@
 package com.example.androidakademija.examples.functions
 
+import com.example.androidakademija.examples.classes.Person
+import com.example.androidakademija.examples.classes.University
 import kotlin.random.Random
 
-object KotlinFunctionExamples {
+val fer = University(123, "FER")
 
-    const val test = 0
+fun getOneRandomNumber(): Int {
+    return Random.nextInt(100)
+}
 
-    fun getOneRandomNumber(): Int {
-        return Random.nextInt(100)
+class Student(
+    firstName: String = "Ivan",
+    lastName: String = "Kovačević",
+    age: Int = 22,
+    oib: Long,
+    private val jmbag: Int,
+    val university: University
+) : Person(firstName, lastName, age, oib) {
+    fun getDetails() = "$firstName $lastName, $age, $oib, $jmbag, ${university.name}"
+}
+
+fun getStudentList() = listOf(
+    Student("Ivana", "Horvat", 22, 987654987654, 1564654335, fer),
+    Student(firstName = "Pero", lastName = "Peric", oib = 123456789123, jmbag = 1655238435, university = fer),
+    Student(firstName = "Pero", oib = 123456789123, jmbag = 1655238435, university = fer),
+    Student(oib = 123456789123, jmbag = 1655238435, university = fer)
+)
+
+fun main() {
+    println(getOneRandomNumber())
+    val list = getStudentList()
+    for (student in list) {
+        println(student.getDetails())
     }
-
-    fun person(name: String, age: Int = 42, isRightHanded: Boolean = false) =
-        name + ", " +  age + ", " + isRightHanded
-
-    fun usePerson() = listOf(
-        person("John"),
-        person("Peter", age = 1),
-        person("Emily", isRightHanded = true),
-        person(name = "Jane", age = 2, isRightHanded = true)
-    )
 }
