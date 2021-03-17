2456a551421c4a4746214913f21fffd29b2b10ab
tox94 <tonibakarcic@gmail.com>
2021-03-17 09:43:39 +0100

    Activity updated.

--- app/src/main/java/com/example/androidakademija/main/MainActivity.kt
+++ app/src/main/java/com/example/androidakademija/main/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_add, R.id.navigation_contacts
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
