2e16cad9857971d96acb2a50b0e68730a9071794
tox94 <tonibakarcic@gmail.com>
2021-03-31 12:14:33 +0200

    Model updated.

--- app/src/main/java/com/example/androidakademija/main/model/Person.kt
+++ app/src/main/java/com/example/androidakademija/main/model/Person.kt
@@ -1,10 +1,17 @@
 package com.example.androidakademija.main.model
 
+import com.example.androidakademija.R
 import java.io.Serializable
 
-class Person constructor(
+class Person(
+    val imageUrl: String,
     val firstName: String,
     val lastName: String,
     val age: Int,
-    val oib: Long
+    val oib: Long,
+    val gender: Gender
 ) : Serializable
+
+enum class Gender(val abbreviation: Int) {
+    MALE(R.string.gender_male_short), FEMALE(R.string.gender_female_short), OTHER(R.string.gender_other_short)
+}
